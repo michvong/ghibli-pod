7e9043dcc5d79790eff6a1be524cafeee753fe66
Michelle Vong <mva30@sfu.ca>
2022-10-10 06:20:16 -0700

    Add handling of break transition

--- src/components/tools/Timer.js
+++ src/components/tools/Timer.js
@@ -71,6 +71,8 @@ export default function Timer() {
     const handleTimerTransition = () => {
         if (isPomodoro) {
             handlePomodoroTransition();
+        } else {
+            handleBreakTransition();
         }
     }
 
