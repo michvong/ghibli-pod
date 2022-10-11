10fdbaa42dda3a36b60f379250d1e16079e444d1
Michelle Vong <mva30@sfu.ca>
2022-10-11 02:33:52 -0700

    Change focus state to active on button click

--- src/components/environment/Environments.js
+++ src/components/environment/Environments.js
@@ -15,10 +15,10 @@ export default function Environments() {
                 </div>
             </div>
 
-            <div class="flex flex-col hover:scrollbar-thin scrollbar-thumb-gray-700 scrollbar-thumb-rounded-md scrollbar-track-rounded-full overflow-x-scroll overflow-hidden">
+            <div class="flex flex-col px-1 hover:scrollbar-thin scrollbar-thumb-gray-700 scrollbar-thumb-rounded-md scrollbar-track-rounded-full overflow-x-scroll overflow-hidden">
                 <div class="flex space-x-6 mt-4 w-25 pb-3">
                     <Tooltip content="mix" placement="bottom">
-                        <button class="flex-shrink-0 relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-2xl group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                        <button class="flex-shrink-0 relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-2xl group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white active:ring-4 active:outline-none active:ring-green-200 dark:active:ring-green-800">
                             <span class="relative p-3 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-2xl group-hover:bg-opacity-0">
                                 <img src={Sparkles} alt="sparkles" class="w-8"/>
                             </span>
@@ -26,7 +26,7 @@ export default function Environments() {
                     </Tooltip>
 
                     <Tooltip content="spirited away" placement="bottom">
-                        <button class="flex-shrink-0 relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-2xl group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                        <button class="flex-shrink-0 relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-2xl group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white active:ring-4 active:outline-none active:ring-green-200 dark:active:ring-green-800">
                             <span class="relative p-3 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-2xl group-hover:bg-opacity-0">
                                 <img src={Dragon} alt="dragon" class="w-8"/>
                             </span>
@@ -34,7 +34,7 @@ export default function Environments() {
                     </Tooltip>
 
                     <Tooltip content="howl's moving castle" placement="bottom">
-                        <button class="flex-shrink-0 relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-2xl group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                        <button class="flex-shrink-0 relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-2xl group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white active:ring-4 active:outline-none active:ring-green-200 dark:active:ring-green-800">
                             <span class="relative p-3 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-2xl group-hover:bg-opacity-0">
                                 <img src={Fire} alt="fire" class="w-8"/>
                             </span>
@@ -42,7 +42,7 @@ export default function Environments() {
                     </Tooltip>
 
                     <Tooltip content="ocean waves" placement="bottom">
-                        <button class="flex-shrink-0 relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-2xl group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                        <button class="flex-shrink-0 relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-2xl group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white active:ring-4 active:outline-none active:ring-green-200 dark:active:ring-green-800">
                             <span class="relative p-3 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-2xl group-hover:bg-opacity-0">
                                 <img src={WaterWave} alt="water wave" class="w-8"/>
                             </span>
