2c093755287eff9646ae040e55eb9272160baa5c
Michelle Vong <mva30@sfu.ca>
2022-10-11 19:40:24 -0700

    Add theme for bug testing

--- src/components/environment/Environment.js
+++ src/components/environment/Environment.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { Tooltip } from '@material-tailwind/react';
 
 export default function Environment({ tooltipContent, image, alt, handleClick}) {
+    // const theme = {
+    //     tooltip: {
+    //         styles: {
+    //             base: {
+    //                 bg: "bg-black",
+    //                 color: "text-white",
+    //             },
+    //         },
+    //     },
+    // };
+
     return (
         <Tooltip content={tooltipContent} placement="bottom">
             <button onClick={handleClick} class="flex-shrink-0 relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-2xl group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white active:ring-4 active:outline-none active:ring-green-200 dark:active:ring-green-800">
