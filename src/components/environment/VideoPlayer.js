7cf1d094eef0dab9ec24f48155af127afd152650
Michelle Vong <mva30@sfu.ca>
2022-10-15 03:08:43 -0700

    Temporarily allow controls to show in video player

--- src/components/environment/VideoPlayer.js
+++ src/components/environment/VideoPlayer.js
@@ -1,4 +1,3 @@
-import { render } from '@testing-library/react';
 import React, { useEffect, useState } from 'react';
 import YouTube from 'react-youtube';
 
@@ -8,18 +7,20 @@ export default function VideoPlayer(playlistId) {
         height: window.innerHeight,
         playerVars: {
             autoplay: 1, // video should auto-play when loaded
-            controls: 0, // don't show user controls
-            disablekb: 1, // don't let user control video with keyboard
+            // controls: 0, // this doesn't work if autoplay is on
+            // disablekb: 1,
             modestbranding: 1,
             list: 'PLI3qJXEG_OMQSMJ7koFkwyGA0Njn4SJKo',
             loop: 1,
-            start: 1,
+            start: 1
         },
     };
 
     return (
-        <YouTube
-            opts={opts}
-        />
+        <div class="pointer-events-none">
+            <YouTube
+                opts={opts}
+            />
+        </div>
     )
 }
