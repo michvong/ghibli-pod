c826fbac5f4ece085c425236928d9b721cd27320
Michelle Vong <mva30@sfu.ca>
2022-10-15 02:16:31 -0700

    Add video player path

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 
 import Timer from './components/timer/Timer';
 import EnvironmentList from './components/environment/EnvironmentList';
+import VideoPlayer from './components/environment/VideoPlayer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         {/* <Route path="/main"></Route> */}
         <Route path="timer" element={<Timer />}></Route>
         <Route path="environments" element={<EnvironmentList />}></Route>
+        <Route path="video-player" element={<VideoPlayer />}></Route>
       </Routes>
     </BrowserRouter>
   );
