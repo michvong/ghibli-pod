9e1039e62757086c3c5d0e40bea9a7a128bd0aae
Michelle Vong <mva30@sfu.ca>
2022-12-24 23:44:24 -0800

    Update playlist Ids

--- src/components/environment/environmentConstants.js
+++ src/components/environment/environmentConstants.js
@@ -12,21 +12,21 @@ export const ENVIRONMENTS = [
   },
 
   {
-    playlistId: 'PLI3qJXEG_OMQSMJ7koFkwyGA0Njn4SJKo',
+    playlistId: 'PLI3qJXEG_OMTb6RGbxBufCi5gBk6R3gnM',
     altText: 'dragon',
     image: Dragon,
     tooltipContent: 'spirited away',
   },
 
   {
-    playlistId: 'PLI3qJXEG_OMQSMJ7koFkwyGA0Njn4SJKo',
+    playlistId: 'PLI3qJXEG_OMTcSS7kVKUuJ7T2GSo54iDH',
     altText: 'fire',
     image: Fire,
     tooltipContent: "howl's moving castle",
   },
 
   {
-    playlistId: 'PLI3qJXEG_OMQSMJ7koFkwyGA0Njn4SJKo',
+    playlistId: 'PLI3qJXEG_OMTWk2udM-jkunwc8t9xx1rv',
     altText: 'water wave',
     image: WaterWave,
     tooltipContent: 'ocean waves',
