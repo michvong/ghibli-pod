772c50abaf253397b8af605dd4605769e10c0094
Michelle Vong <mva30@sfu.ca>
2022-12-25 05:12:12 -0800

    Add icons to sidebar

--- src/components/Container.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Container({ width, height }) {
-    return (
-        <div class={"px-" + (width) + " py-" + (height) + "block bg-gray-900 border border-gray-800 shadow-xl rounded-xl max-w-xs"}></div>
-    )
-}
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Up from '../../assets/icons/chevron-up.svg';
 import Down from '../../assets/icons/chevron-down.svg';
+import Clock from '../../assets/icons/clock.svg';
+import Image from '../../assets/icons/image.svg';
 
 export default function Sidebar() {
   const [showSidebar, setShowSidebar] = useState(false);
@@ -17,19 +19,34 @@ export default function Sidebar() {
       </button>
 
       <div
-        className={`top-0 left-0 bg-gray-900 p-3 text-white fixed h-full z-40 ease-in-out duration-500 ${
+        className={`flex flex-col content-center justify-start top-0 left-0 bg-gray-900 p-3 text-white fixed h-full z-40 ease-in-out duration-500 ${
           showSidebar ? 'translate-x-0 ' : 'translate-y-full'
         }`}
       >
-        <button
-          className="flex text-4xl text-white items-center cursor-pointer mt-2 z-50
-            bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300
-            font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:bg-blue-600
-            dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          onClick={() => setShowSidebar(!showSidebar)}
-        >
-          <img src={Down} />
-        </button>
+        <div class="flex justify-center">
+          <button
+            className="cursor-pointer mt-2 z-50 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+              focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center
+              dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={() => setShowSidebar(!showSidebar)}
+          >
+            <img src={Down} />
+          </button>
+        </div>
+
+        <div class="flex flex-col justify-center items-center mt-7">
+          <button>
+            <img src={Clock} />
+          </button>
+          <p class="text-sm">timer</p>
+        </div>
+
+        <div class="flex flex-col justify-center items-center mt-4">
+          <button>
+            <img src={Image} />
+          </button>
+          <p class="text-sm">spaces</p>
+        </div>
       </div>
     </>
   );
