e9c24fad3a64ed9a34e81f7e0df5d25b8e17a7fb
Michelle Vong <mva30@sfu.ca>
2022-12-24 19:43:53 -0800

    Add video methods, make environment list draggable

--- src/components/environment/VideoPlayer.js
+++ src/components/environment/VideoPlayer.js
@@ -1,26 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import YouTube from 'react-youtube';
+import EnvironmentList from './EnvironmentList';
+import Draggable from 'react-draggable';
 
-export default function VideoPlayer({ playlistId }) {
-    const opts = {
-        width: window.innerWidth,
-        height: window.innerHeight,
-        playerVars: {
-            autoplay: 1, // video should auto-play when loaded
-            // controls: 0, // this doesn't work if autoplay is on
-            // disablekb: 1,
-            modestbranding: 1,
-            list: playlistId,
-            loop: 1,
-            start: 1
-        },
-    };
+export default function VideoPlayer() {
+  const [currentPlaylistId, setCurrentPlaylistId] = useState();
 
-    return (
-        <div class="pointer-events-none">
-            <YouTube
-                opts={opts}
-            />
+  const opts = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    playerVars: {
+      autoplay: 1, // video should auto-play when loaded
+      // controls: 0, // this doesn't work if autoplay is on
+      // disablekb: 1,
+      modestbranding: 1,
+      list: currentPlaylistId,
+      loop: 1,
+      start: 1,
+    },
+  };
+
+  const handlePlaylistSelect = (playlistId) => {
+    setCurrentPlaylistId(playlistId);
+  };
+
+  return (
+    <div>
+      <div class="absolute pointer-events-none">
+        <YouTube opts={opts} />
+      </div>
+
+      <Draggable>
+        <div class="box">
+          <EnvironmentList class="absolute" handlePlaylistSelect={handlePlaylistSelect} />
         </div>
-    )
+      </Draggable>
+    </div>
+  );
 }
