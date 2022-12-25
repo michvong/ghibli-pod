77de848189ea930fd512675575e7e8f13eb0e2d6
Michelle Vong <mva30@sfu.ca>
2022-12-24 19:42:53 -0800

    Move playlists to constants file

--- src/components/environment/EnvironmentList.js
+++ src/components/environment/EnvironmentList.js
@@ -1,85 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import api from '../../services/api'
-import { Tooltip } from '@material-tailwind/react';
-
-import Sparkles from '../../assets/icons/sparkles.png'
-import Dragon from '../../assets/icons/dragon.png'
-import Fire from '../../assets/icons/fire.png'
-import WaterWave from '../../assets/icons/water_wave.png'
 import Environment from './Environment';
-import VideoPlayer from './VideoPlayer';
-import { render } from '@testing-library/react';
+import { ENVIRONMENTS } from './environmentConstants';
 
-export default function EnvironmentList() {
-    const mixPlaylistId = 'PLI3qJXEG_OMQSMJ7koFkwyGA0Njn4SJKo';
-    // const spiritedAwayPlaylistId = 
-    const [results, setResults] = useState([]);
-    const [isVideoPlaying, setIsVideoPlaying] = useState(false);
-    const [currentPlaylistId, setCurrentPlaylistId] = useState();
+export default function EnvironmentList({ handlePlaylistSelect }) {
+  const onPlaylistSelect = (playlistId) => {
+    handlePlaylistSelect(playlistId);
+  };
 
-    // useEffect(() => {
-    //     api.getVideoIdFromPlaylist("PLI3qJXEG_OMQSMJ7koFkwyGA0Njn4SJKo", 0)
-    //       .then((response) => {
-    //         // setResults(response.data.items[0].contentDetails.videoId);
-    //         setResults(response);
-    //       })
-    //   }, []);
-
-    const handleMixClick = () => {
-        // console.log(api.getPlaylistInfo("PLI3qJXEG_OMQSMJ7koFkwyGA0Njn4SJKo"));
-        // console.log(api.getPlaylistContent("PLI3qJXEG_OMQSMJ7koFkwyGA0Njn4SJKo"));
-        // console.log(results);
-        setIsVideoPlaying(true);
-        console.log("The video should play.")
-        setCurrentPlaylistId(mixPlaylistId);
-        // render(<VideoPlayer playlistId={mixPlaylistId} />);
-    }
-
-    return (
-        <div class="relative h-screen w-screen">
-            <div class={(isVideoPlaying ? 'visible' : 'invisible')}>
-                <VideoPlayer playlistId={currentPlaylistId} />
-            </div>
-
-            <div class="block px-5 py-5 bg-gray-900 border border-gray-800 shadow-xl rounded-xl max-w-xs">
-                <div class="flex justify-between">
-                    <div class="flex justify-start">
-                        <h3 class="text-xl text-white">environments</h3>
-                    </div>
-                </div>
-
-                <div class="flex flex-col px-1 hover:scrollbar-thin scrollbar-thumb-gray-700 scrollbar-thumb-rounded-md scrollbar-track-rounded-full overflow-x-scroll overflow-hidden">
-                    <div class="flex space-x-6 mt-4 w-25 pb-3">
-                        <Environment
-                            tooltipContent="mix"
-                            image={Sparkles}
-                            alt="sparkles"
-                            handleClick={handleMixClick}
-                        />
-
-                        <Environment
-                            tooltipContent="spirited away"
-                            image={Dragon}
-                            alt="dragon"
-                            // handleClick={handleMixClick}
-                        />
-
-                        <Environment
-                            tooltipContent="howl's moving castle"
-                            image={Fire}
-                            alt="fire"
-                            // handleClick={handleMixClick}
-                        />
-
-                        <Environment
-                            tooltipContent="ocean waves"
-                            image={WaterWave}
-                            alt="water wave"
-                            // handleClick={handleMixClick}
-                        />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div class>
+      <div class="block relative px-5 py-5 bg-gray-900 border border-gray-800 shadow-xl rounded-xl max-w-xs">
+        <div class="flex justify-between">
+          <div class="flex justify-start">
+            <h3 class="text-xl text-white">environments</h3>
+          </div>
         </div>
-    )
+
+        <div class="flex flex-col px-1 hover:scrollbar-thin scrollbar-thumb-gray-700 scrollbar-thumb-rounded-md scrollbar-track-rounded-full overflow-x-scroll overflow-hidden">
+          <div class="flex space-x-6 mt-4 w-25 pb-3">
+            {ENVIRONMENTS.map((env) => (
+              <Environment
+                tooltipContent={env.tooltipContent}
+                image={env.image}
+                alt={env.altText}
+                handleClick={() => onPlaylistSelect(env.playlistId)}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/environment/environmentConstants.js
@@ -0,0 +1,34 @@
+import Sparkles from '../../assets/icons/sparkles.png';
+import Dragon from '../../assets/icons/dragon.png';
+import Fire from '../../assets/icons/fire.png';
+import WaterWave from '../../assets/icons/water_wave.png';
+
+export const ENVIRONMENTS = [
+  {
+    playlistId: 'PLI3qJXEG_OMQSMJ7koFkwyGA0Njn4SJKo',
+    altText: 'sparkles',
+    image: Sparkles,
+    tooltipContent: 'mix',
+  },
+
+  {
+    playlistId: 'PLI3qJXEG_OMQSMJ7koFkwyGA0Njn4SJKo',
+    altText: 'dragon',
+    image: Dragon,
+    tooltipContent: 'spirited away',
+  },
+
+  {
+    playlistId: 'PLI3qJXEG_OMQSMJ7koFkwyGA0Njn4SJKo',
+    altText: 'fire',
+    image: Fire,
+    tooltipContent: "howl's moving castle",
+  },
+
+  {
+    playlistId: 'PLI3qJXEG_OMQSMJ7koFkwyGA0Njn4SJKo',
+    altText: 'water wave',
+    image: WaterWave,
+    tooltipContent: 'ocean waves',
+  },
+];
