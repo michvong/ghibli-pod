c3c97cdc37ebfd94068e3466d79a6229f4d1fbda
Michelle Vong <mva30@sfu.ca>
2023-05-08 16:34:04 -0700

    Add checkbox for to-do list

--- src/components/todo/ToDoList.js
+++ src/components/todo/ToDoList.js
@@ -1,16 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { Checkbox } from '@material-tailwind/react';
 
 export default function ToDoList({ handleToDoClick }) {
+  const [newTask, setNewTask] = useState('');
+
+  const [tasks, setTasks] = useState([]);
+
   const onMinimize = () => {
     handleToDoClick();
   };
 
+  const handleTaskClick = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+  };
+
+  const handleDeleteClick = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
   return (
     <div class>
-      <div class="block relative px-5 py-5 bg-gray-900 border border-gray-800 shadow-xl rounded-xl max-w-xs">
+      <div class="block px-5 py-5 bg-gray-900 border border-gray-800 shadow-xl rounded-xl max-w-xs">
         <div class="flex justify-between">
           <div class="flex justify-start">
-            <h3 class="text-xl text-white">to-do</h3>
+            <h3 class="mr-4 text-xl text-white">to-do</h3>
           </div>
 
           <button onClick={onMinimize}>
@@ -31,8 +48,58 @@ export default function ToDoList({ handleToDoClick }) {
           </button>
         </div>
 
-        <div class="flex flex-col px-1 mb-4 hover:scrollbar-thin scrollbar-thumb-gray-700 scrollbar-thumb-rounded-md scrollbar-track-rounded-full overflow-y-scroll overflow-hidden">
-          <div class="flex space-x-6 mt-4 w-25 pb-3">hello</div>
+        <div class="flex flex-col mt-3 mb-4 hover:scrollbar-thin scrollbar-thumb-gray-700 scrollbar-thumb-rounded-md scrollbar-track-rounded-full overflow-y-scroll overflow-hidden">
+          <div class="mr-4">
+            {tasks.map((task, index) => (
+              <div key={index} class="flex items-center">
+                <Checkbox
+                  class="p-3"
+                  checked={task.completed}
+                  onChange={() => handleTaskClick(index)}
+                  color="indigo"
+                />
+
+                <input
+                  type="text"
+                  placeholder="enter your task"
+                  class={`mr-3 px-2 py-0.5 bg-gray-800 w-full rounded-md text-white/50 text-base ${
+                    task.completed ? 'line-through text-gray-400' : ''
+                  }`}
+                  value={newTask}
+                  onChange={(e) => setNewTask(e.target.value)}
+                />
+
+                <button onClick={() => handleDeleteClick(index)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="22"
+                    height="22"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#ffffff"
+                    stroke-width="1"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <polyline points="3 6 5 6 21 6"></polyline>
+                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                    <line x1="10" y1="11" x2="10" y2="17"></line>
+                    <line x1="14" y1="11" x2="14" y2="17"></line>
+                  </svg>
+                </button>
+              </div>
+            ))}
+
+            <button
+              class="flex justify-between pl-4 py-1 w-full rounded-lg text-white rounded-md hover:bg-white/10 text-white/50 text-base"
+              onClick={() => {
+                setTasks([...tasks, { task: newTask, completed: false }]);
+                setNewTask('');
+              }}
+            >
+              + <span class="text-white/50 mr-1.5">add a new task</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
