3713eb7e927f67f0a7efd77961e7e6aeb8a13460
Michelle Vong <mva30@sfu.ca>
2023-05-08 17:06:10 -0700

    Add to-do functionality

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
-import ToDoList from './components/todo/ToDoList';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}></Route>
-        <Route path="to-do" element={<ToDoList />}></Route>
       </Routes>
     </BrowserRouter>
   );
